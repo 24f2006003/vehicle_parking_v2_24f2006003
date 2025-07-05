f2be0d9bc166a00765e51ff0ab493b7e934fbebb
24f2006003 <24f2006003@ds.study.iitm.ac.in>
2025-07-05 18:12:36 +0530

    Update routes.py

--- backend/application/routes.py
+++ backend/application/routes.py
@@ -2,22 +2,22 @@ from flask import current_app as app, jsonify, request, abort
 from .models import User
 from flask_jwt_extended import create_access_token, current_user, jwt_required
 
+
 def role_required(required_role):
     def wrapper(fn):
         @jwt_required()
         def decorator(*args, **kwargs):
             if current_user.role != required_role:
-                return jsonify({"msg": "Unauthorized"}), 403
+                return jsonify(message="Unauthorized"), 403
             return fn(*args, **kwargs)
         return decorator
     return wrapper
 
 
-
-@app.route('/login', methods=['POST'])
+@app.route("/login", methods=["POST"])
 def login():
-    username = request.json.get('username', None)
-    password = request.json.get('password', None)
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
 
     user = User.query.filter_by(username=username).one_or_none()
     if not user or not user.password == password:
@@ -25,6 +25,7 @@ def login():
 
     access_token = create_access_token(identity=user)
     return jsonify(access_token=access_token)
+
 
 # @app.route('/who_am_i', methods=['GET'])
 # @jwt_required()
@@ -36,6 +37,7 @@ def login():
 #         password=current_user.password,
 #         role=current_user.role
 #     )
+
 
 @app.route("/dashboard", methods=["GET"])
 @jwt_required()
